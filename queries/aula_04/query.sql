6e2be5ef681429530bccdceb1da26b9e96b4d97b
zlucasftw <ilucasmcpvp@gmail.com>
2025-01-19 19:38:33 -0300

    wip: Aula 04 - Testes automatizados - 01, 02, 04 - Testes com Spring Boot, Configurando o banco de testes, Testando o repository

---
+++ queries/aula_04/query.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE vollmed_api_test;
---
+++ src/test/java/exemplo/aula_04/memory_database/MedicoRepositoryTest.java
@@ -0,0 +1,14 @@
+package exemplo.aula_04.memory_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class MedicoRepositoryTest {
+
+    @Test
+    void escolherMedicoAleatorioLivreNaData() {
+
+    }
+
+}
